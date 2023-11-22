342c4d74fd190e446231e5367b1288da8d423256
Deepen Dhulla <deepen@deependhulla.com>
2023-11-22 18:23:00 +0530

    update on Wed Nov 22 06:23:00 PM IST 2023

--- 01-repo-for-debian-12.sh
+++ 01-repo-for-debian-12.sh
@@ -3,6 +3,13 @@
 # for Debian 12
 ## set to India IST timezone -- You can dissable it if needed
 timedatectl set-timezone 'Asia/Kolkata'
+dpkg-reconfigure -f noninteractive tzdata
+
+sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+sed -i -e 's/en_IN UTF-8/# en_IN UTF-8/' /etc/locale.gen
+locale-gen en_US en_US.UTF-8
+echo "LANG=en_US.UTF-8" > /etc/environment
+echo "LC_ALL=en_US.UTF-8" >> /etc/environment
 
 ##disable ipv6 as most time not required
 sysctl -w net.ipv6.conf.all.disable_ipv6=1 1>/dev/null
@@ -31,8 +38,12 @@ echo "iptables-persistent iptables-persistent/autosave_v6 boolean true" | debcon
 DEBIAN_FRONTEND=noninteractive
 
 apt -y install vim chrony openssh-server screen net-tools git mc postfix sendemail  \
-sudo wget curl ethtool iptraf-ng traceroute telnet rsyslog
+sudo wget curl ethtool iptraf-ng traceroute telnet rsyslog software-properties-common \
+dirmngr parted gdisk apt-transport-https lsb-release ca-certificates iputils-ping \
+debconf-utils pwgen xfsprogs iftop htop multitail net-tools elinks pssh
+
 systemctl restart chrony
+systemctl restart rsyslog
 
 ## centos like bash ..for all inteactive 
 echo "" >> /etc/bash.bashrc
@@ -91,5 +102,30 @@ echo "syntax on" >> /etc/skel/.vimrc
 sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin yes/g" /etc/ssh/sshd_config
 #sed -i "s/#Port 22/Port 7722/g" /etc/ssh/sshd_config
 systemctl restart ssh
+
+
+
+##### configure proper timezone
+#dpkg-reconfigure tzdata
+##### configure locale proper
+#dpkg-reconfigure locales
+## set India IST time.
+#/bin/rm -rf /etc/localtime
+#/bin/ln -vs /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
+#### for adding firmware realtek driver
+#apt-get install firmware-linux-nonfree
+#apt-get install firmware-realtek
+#update-initramfs -u
+## only if VM notfor LXC
+## for proxmox/kvm better preformance
+#apt-get -y install qemu-guest-agent
+## if on Consle need Mouse to use for copy paste use gpm
+#apt-get install gpm
+#google dns: [2001:4860:4860::8888]
+#cloudflare dns: [2606:4700:4700::1111]
+
+
+hostname -f
+ping `hostname -f` -c 2
 
 

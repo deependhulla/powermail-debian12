dda1913c29c6d26c09ce9c63fc8768fed26080e3
Deepen Dhulla <deepen@deependhulla.com>
2023-11-22 18:02:03 +0530

    update on Wednesday 22 November 2023 06:02:03 PM IST

---
+++ 01-repo-for-debian-12.sh
@@ -0,0 +1,95 @@
+#!/bin/sh
+
+# for Debian 12
+## set to India IST timezone -- You can dissable it if needed
+timedatectl set-timezone 'Asia/Kolkata'
+
+##disable ipv6 as most time not required
+sysctl -w net.ipv6.conf.all.disable_ipv6=1 1>/dev/null
+sysctl -w net.ipv6.conf.default.disable_ipv6=1 1>/dev/null
+
+## backup existing repo by copy just for safety
+mkdir -p /opt/old-config-backup/ 2>/dev/null
+/bin/cp -pR /etc/apt/sources.list /opt/old-config-backup/old-sources.list-`date +%s`
+echo "" >  /etc/apt/sources.list
+
+echo "deb http://deb.debian.org/debian/ bookworm main non-free-firmware contrib non-free" >> /etc/apt/sources.list
+echo "deb-src http://deb.debian.org/debian/ bookworm main non-free-firmware contrib non-free" >> /etc/apt/sources.list
+echo "deb http://security.debian.org/debian-security bookworm-security main non-free-firmware contrib non-free" >> /etc/apt/sources.list
+echo "deb-src http://security.debian.org/debian-security bookworm-security main non-free-firmware contrib non-free" >> /etc/apt/sources.list
+echo "deb http://deb.debian.org/debian/ bookworm-updates main non-free-firmware contrib non-free" >> /etc/apt/sources.list
+echo "deb-src http://deb.debian.org/debian/ bookworm-updates main non-free-firmware contrib non-free" >> /etc/apt/sources.list
+
+apt update
+apt -y upgrade
+
+CFG_HOSTNAME_FQDN=`hostname -f`
+echo "postfix postfix/main_mailer_type select Internet Site" | debconf-set-selections
+echo "postfix postfix/mailname string $CFG_HOSTNAME_FQDN" | debconf-set-selections
+echo "iptables-persistent iptables-persistent/autosave_v4 boolean true" | debconf-set-selections
+echo "iptables-persistent iptables-persistent/autosave_v6 boolean true" | debconf-set-selections
+DEBIAN_FRONTEND=noninteractive
+
+apt -y install vim chrony openssh-server screen net-tools git mc postfix sendemail  \
+sudo wget curl ethtool iptraf-ng traceroute telnet rsyslog
+systemctl restart chrony
+
+## centos like bash ..for all inteactive 
+echo "" >> /etc/bash.bashrc
+echo "alias cp='cp -i'" >> /etc/bash.bashrc
+echo "alias l.='ls -d .* --color=auto'" >> /etc/bash.bashrc
+echo "alias ll='ls -l --color=auto'" >> /etc/bash.bashrc
+echo "alias ls='ls --color=auto'" >> /etc/bash.bashrc
+echo "alias mv='mv -i'" >> /etc/bash.bashrc
+echo "alias rm='rm -i'" >> /etc/bash.bashrc
+echo "export EDITOR=vi" >> /etc/bash.bashrc
+#echo "export LC_CTYPE=en_US.UTF-8" >> /etc/bash.bashrc
+#echo "export LC_ALL=en_US.UTF-8" >> /etc/bash.bashrc
+
+#Setting rc.local, perl, bash, vim basic default config and IST time sync NTP
+
+/bin/cp -pR /etc/rc.local /opt/old-config-backup/old-rc.local-`date +%s` 2>/dev/null
+## create with default IPV6 disabled
+touch /etc/rc.local
+printf '%s\n' '#!/bin/bash'  | tee -a /etc/rc.local 1>/dev/null
+echo "sysctl -w net.ipv6.conf.all.disable_ipv6=1" >>/etc/rc.local
+echo "sysctl -w net.ipv6.conf.default.disable_ipv6=1" >> /etc/rc.local
+echo "sysctl vm.swappiness=0" >> /etc/rc.local
+echo "exit 0" >> /etc/rc.local
+chmod 755 /etc/rc.local
+## need like autoexe bat on startup
+echo "[Unit]" > /etc/systemd/system/rc-local.service
+echo " Description=/etc/rc.local Compatibility" >> /etc/systemd/system/rc-local.service
+echo " ConditionPathExists=/etc/rc.local" >> /etc/systemd/system/rc-local.service
+echo "" >> /etc/systemd/system/rc-local.service
+echo "[Service]" >> /etc/systemd/system/rc-local.service
+echo " Type=forking" >> /etc/systemd/system/rc-local.service
+echo " ExecStart=/etc/rc.local start" >> /etc/systemd/system/rc-local.service
+echo " TimeoutSec=0" >> /etc/systemd/system/rc-local.service
+echo " StandardOutput=tty" >> /etc/systemd/system/rc-local.service
+echo " RemainAfterExit=yes" >> /etc/systemd/system/rc-local.service
+## featured Removed
+###echo " SysVStartPriority=99" >> /etc/systemd/system/rc-local.service
+echo "" >> /etc/systemd/system/rc-local.service
+echo "[Install]" >> /etc/systemd/system/rc-local.service
+echo " WantedBy=multi-user.target" >> /etc/systemd/system/rc-local.service
+
+systemctl enable rc-local
+systemctl start rc-local
+
+## make cpan auto yes for pre-requist modules of perl
+(echo y;echo o conf prerequisites_policy follow;echo o conf commit)|cpan 1>/dev/null
+
+#Disable vim automatic visual mode using mouse
+echo "\"set mouse=a/g" >  ~/.vimrc
+echo "syntax on" >> ~/.vimrc
+##  for  other new users
+echo "\"set mouse=a/g" >  /etc/skel/.vimrc
+echo "syntax on" >> /etc/skel/.vimrc
+
+##Comment this if you do not want root login via ssh activated using port 7722
+sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin yes/g" /etc/ssh/sshd_config
+#sed -i "s/#Port 22/Port 7722/g" /etc/ssh/sshd_config
+systemctl restart ssh
+
+

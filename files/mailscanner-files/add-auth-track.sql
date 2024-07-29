2d9a21c0773ed0fa0a206f8f3c6bfb3c7f8cea78
Deepen Dhulla <deepen@deependhulla.com>
2024-07-29 15:31:04 +0530

    Update add-auth-track.sql
    updated central_done column.

--- files/mailscanner-files/add-auth-track.sql
+++ files/mailscanner-files/add-auth-track.sql
@@ -6,3 +6,8 @@ CREATE TABLE IF NOT EXISTS mailscanner.`maillog_auth` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 COMMIT;
 
+ALTER TABLE mailscanner.`maillog` ADD `central_done` TINYINT NOT NULL DEFAULT '0' AFTER `last_update`, ADD INDEX (`central_done`);
+ALTER TABLE mailscanner.`maillog_auth` ADD `central_done` TINYINT NOT NULL DEFAULT '0' AFTER `clientauth`, ADD INDEX (`central_done`);
+ALTER TABLE mailscanner.`mtalog_ids` ADD `central_done` TINYINT NOT NULL DEFAULT '0' AFTER `smtp_id`, ADD INDEX (`central_done`);
+ALTER TABLE mailscanner.`mtalog` ADD `central_done` TINYINT NOT NULL DEFAULT '0' AFTER `delay`, ADD INDEX (`central_done`);
+
